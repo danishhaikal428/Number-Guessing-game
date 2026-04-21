614a0dce9f37e9669652005cca251a414de59421
danishhaikal428 <danishhaikal0129@gmail.com>
2026-04-21 22:11:04 +0800

    first commit

---
+++ numberguesser.py
@@ -0,0 +1,46 @@
+attempts = 0
+
+import random
+number = random.randint(1, 100)
+
+while True:
+    guess = input("Guess a number (Type 'exit' to quit): ")
+    if guess == 'exit':
+        break
+
+    guess = int(guess)
+    attempts +=1
+
+    if guess == number:
+        print(f"Congratss!! You got it with {attempts} attempts!! 🎉")
+        play_again = input("Play again? (yes/no): ")
+        if play_again == 'yes':
+            attempts = 0
+            number = random.randint(1, 100)
+        else:
+            break
+    elif guess > number:
+        print(f"Too high!! You have {10 - attempts}attempts left!!!")
+    else:
+        print(f"Too low!! You have {10 - attempts}attempts left!!!")
+    
+    if attempts == 10:
+        print("Sudah lah tu 10 kali sudah tak dapat ii lagi apalahhh")
+        print(f"Game over! The number was {number}.")
+        play_again = input("Play again? (yes/no): ")
+        if play_again == 'yes':
+            attempts = 0
+            number = random.randint(1, 100)
+        else:
+            break
+
+
+
+
+
+
+
+
+
+
+
